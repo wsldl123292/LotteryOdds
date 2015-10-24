706474ff923349ef1b9897c34d28066216cd663b
LDL <wsldl123292@163.com>
2015-10-24 14:06:21 +0800

    no message

--- src/main/java/com/ldl/lotteryodds/train/TestTextDXAll.java
+++ src/main/java/com/ldl/lotteryodds/train/TestTextDXAll.java
@@ -32,8 +32,8 @@ import java.util.Map;
 public class TestTextDXAll {
     public static void main(String[] args) throws IOException {
 //采集开始时间2011-07-21
-        LocalDate beginDate = LocalDate.of(2015, 10, 17);
-        int size = 8;
+        LocalDate beginDate = LocalDate.of(2015, 10, 21);
+        int size = 12;
         final CloseableHttpClient client = HttpClientBuilder.create().build();
         CloseableHttpResponse response;
         HttpGet get;
