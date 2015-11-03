995f6da55282d8a9ef26ac2c221d3769a0cd31b1
LDL <wsldl123292@163.com>
2015-11-03 18:33:17 +0800

    no message

--- src/main/java/com/ldl/lotteryodds/train/scala/TestBinary.scala
+++ src/main/java/com/ldl/lotteryodds/train/scala/TestBinary.scala
@@ -8,47 +8,47 @@ import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.mllib.regression.LabeledPoint
 
 /**
- * 作者: LDL
- * 功能说明: 随机森林分类数据
- * 创建日期: 2015/9/30 14:37
- */
+  * 作者: LDL
+  * 功能说明: 随机森林分类数据
+  * 创建日期: 2015/9/30 14:37
+  */
 object TestBinary {
-    def main(args: Array[ String ]) {
-        System.setProperty( "hadoop.home.dir", "F:\\data\\hadoop-common-2.2.0-bin-master" )
-        val sc = new SparkContext( "local[4]", "am" )
+    def main(args: Array[String]) {
+        System.setProperty("hadoop.home.dir", "F:\\data\\hadoop-common-2.2.0-bin-master")
+        val sc = new SparkContext("local[4]", "am")
 
         /** 测试数据 */
-        val testRowData = sc.textFile( "F:\\data\\lotteryodds\\test_all_binary.txt" )
-        val testRecords = testRowData.map( line => line.split( "\t" ) )
+        val testRowData = sc.textFile("F:\\data\\lotteryodds\\test_all_binary.txt")
+        val testRecords = testRowData.map(line => line.split("\t"))
         val testData = testRecords.map { r =>
-            val trimmed = r.map( _.replaceAll( "\"", "" ) )
-            val label = trimmed( r.size - 1 ).toInt
-            val features = trimmed.slice( 0, r.size - 1 ).map( d => if(d == null) 0 else d.toDouble )
-            LabeledPoint( label, Vectors.dense(features))
+            val trimmed = r.map(_.replaceAll("\"", ""))
+            val label = trimmed(r.size - 1).toInt
+            val features = trimmed.slice(0, r.size - 1).map(d => if (d == null) 0 else d.toDouble)
+            LabeledPoint(label, Vectors.dense(features))
         }
-        testData.cache( )
+        testData.cache()
 
-        val model = LogisticRegressionModel.load( sc, "F:\\data\\lotteryodds\\model\\LogisticRegressionBinary" )
+        val model = LogisticRegressionModel.load(sc, "F:\\data\\lotteryodds\\model\\LogisticRegressionBinary")
         model.setThreshold(0.56)
         val labelAndPreds = testData.map { point =>
             val prediction = model.predict(point.features)
             (point.label, prediction)
         }
-        print("label : ",labelAndPreds.collect().toList)
-        val testErr = labelAndPreds.filter( r => r._1 != r._2 ).count().toDouble / testData.count()
+        print("label : ", labelAndPreds.collect().toList)
+        val testErr = labelAndPreds.filter(r => r._1 != r._2).count().toDouble / testData.count()
         println("Test Error = " + testErr)
 
-        val writer = new PrintWriter(new File("F:\\data\\lotteryodds\\result_all_binary.txt" ))
-        val predictions = testData.map { point => model.predict( point.features ) }
-        predictions.collect().toList.foreach( p => {
-            if(p.toInt==0){
+        val writer = new PrintWriter(new File("F:\\data\\lotteryodds\\result_all_binary.txt"))
+        val predictions = testData.map { point => model.predict(point.features) }
+        predictions.collect().toList.foreach(p => {
+            if (p.toInt == 0) {
                 writer.write("预测结果 : 3 \n")
-            }else{
+            } else {
                 writer.write("预测结果 : 01 \n")
             }
-        } )
+        })
         writer.close()
 
-        sc.stop( )
+        sc.stop()
     }
 }
--- src/main/java/com/ldl/lotteryodds/train/scala/TrainBinary.scala
+++ src/main/java/com/ldl/lotteryodds/train/scala/TrainBinary.scala
@@ -9,22 +9,22 @@ import org.apache.spark.mllib.optimization.{LBFGS, LogisticGradient, SquaredL2Up
 import org.apache.spark.mllib.util.MLUtils
 
 /**
- * 作者: LDL
- * 说明:
- * 时间: 2015/10/6 21:11
- */
+  * 作者: LDL
+  * 说明:
+  * 时间: 2015/10/6 21:11
+  */
 object TrainBinary {
-    def main(args: Array[ String ]) {
-        System.setProperty( "hadoop.home.dir", "F:\\data\\hadoop-common-2.2.0-bin-master" )
-        val sc = new SparkContext( "local[5]", "am" )
+    def main(args: Array[String]) {
+        System.setProperty("hadoop.home.dir", "F:\\data\\hadoop-common-2.2.0-bin-master")
+        val sc = new SparkContext("local[5]", "am")
         /** 训练数据 */
-        val trainRowDataAll = sc.textFile( "F:\\data\\lotteryodds\\train_all_binary.txt" )
-        val trainRecordsAll = trainRowDataAll.map( line => line.split( "\t" ) )
+        val trainRowDataAll = sc.textFile("F:\\data\\lotteryodds\\train_all_binary.txt")
+        val trainRecordsAll = trainRowDataAll.map(line => line.split("\t"))
         val trainingData = trainRecordsAll.map { r =>
-            val trimmed = r.map( _.replaceAll( "\"", "" ) )
-            val label = trimmed( r.size - 1 ).toInt
-            val features = trimmed.slice( 0, r.size - 1 ).map( d => if ( d == "" ) 0.00 else d.toDouble )
-            LabeledPointCus( "", label, Vectors.dense( features ) )
+            val trimmed = r.map(_.replaceAll("\"", ""))
+            val label = trimmed(r.size - 1).toInt
+            val features = trimmed.slice(0, r.size - 1).map(d => if (d == "") 0.00 else d.toDouble)
+            LabeledPointCus("", label, Vectors.dense(features))
         }
 
         /*val splits = trainDataAll.randomSplit(Array(0.9, 0.1))
@@ -32,16 +32,16 @@ object TrainBinary {
         trainingData.cache()*/
 
 
-        val testRowData = sc.textFile( "F:\\data\\lotteryodds\\test_all_binary.txt" )
-        val testRecords = testRowData.map( line => line.split( "\t" ) )
+        val testRowData = sc.textFile("F:\\data\\lotteryodds\\test_all_binary.txt")
+        val testRecords = testRowData.map(line => line.split("\t"))
         val testData = testRecords.map { r =>
-            val trimmed = r.map( _.replaceAll( "\"", "" ) )
-            val num = trimmed( r.size - 1 )
-            val label = trimmed( r.size - 2 ).toInt
-            val features = trimmed.slice( 0, r.size - 2 ).map( d => if ( d == null ) 0 else d.toDouble )
-            LabeledPointCus( num, label, Vectors.dense( features ) )
+            val trimmed = r.map(_.replaceAll("\"", ""))
+            val num = trimmed(r.size - 1)
+            val label = trimmed(r.size - 2).toInt
+            val features = trimmed.slice(0, r.size - 2).map(d => if (d == null) 0 else d.toDouble)
+            LabeledPointCus(num, label, Vectors.dense(features))
         }
-        testData.cache( )
+        testData.cache()
 
 
         /** 分类 */
@@ -62,52 +62,52 @@ object TrainBinary {
 
         model.save(sc,"F:\\data\\lotteryodds\\model\\LogisticRegressionBinary")*/
 
-        val training = trainingData.map( x => (x.label, MLUtils.appendBias( x.features )) ).cache( )
-        val numFeatures = trainingData.take( 1 )( 0 ).features.size
+        val training = trainingData.map(x => (x.label, MLUtils.appendBias(x.features))).cache()
+        val numFeatures = trainingData.take(1)(0).features.size
         val numCorrections = 10
         val convergenceTol = 1e-4
         val maxNumIterations = 30
         val regParam = 0.001
-        val initialWeightsWithIntercept = Vectors.dense( new Array[ Double ]( numFeatures + 1 ) )
+        val initialWeightsWithIntercept = Vectors.dense(new Array[Double](numFeatures + 1))
 
         val (weightsWithIntercept, loss) = LBFGS.runLBFGS(
             training,
-            new LogisticGradient( ),
-            new SquaredL2Updater( ),
+            new LogisticGradient(),
+            new SquaredL2Updater(),
             numCorrections,
             convergenceTol,
             maxNumIterations,
             regParam,
-            initialWeightsWithIntercept )
+            initialWeightsWithIntercept)
 
         val model = new LogisticRegressionModel(
-            Vectors.dense( weightsWithIntercept.toArray.slice( 0, weightsWithIntercept.size - 1 ) ),
-            weightsWithIntercept( weightsWithIntercept.size - 1 ) )
+            Vectors.dense(weightsWithIntercept.toArray.slice(0, weightsWithIntercept.size - 1)),
+            weightsWithIntercept(weightsWithIntercept.size - 1))
 
-        model.setThreshold( 0.56 )
+        model.setThreshold(0.56)
 
-        val writer = new PrintWriter( new File( "F:\\data\\lotteryodds\\result_all_binary30.txt" ) )
+        val writer = new PrintWriter(new File("F:\\data\\lotteryodds\\result_all_binary30.txt"))
 
-        val scoreAndLabels = testData.map { case LabeledPointCus( num, label, features ) =>
-            val score = model.predict( features )
+        val scoreAndLabels = testData.map { case LabeledPointCus(num, label, features) =>
+            val score = model.predict(features)
 
             (num, label, score)
         }
-        println( scoreAndLabels.collect( ).toList )
-        scoreAndLabels.collect( ).toList.foreach( p => {
-            if ( p._3.toInt == 0 ) {
-                writer.write( p._1 + " : 3 \n" )
+        println(scoreAndLabels.collect().toList)
+        scoreAndLabels.collect().toList.foreach(p => {
+            if (p._3.toInt == 0) {
+                writer.write(p._1 + " : 3 \n")
             } else {
-                writer.write( p._1 + " : 01 \n" )
+                writer.write(p._1 + " : 01 \n")
             }
-        } )
-        writer.close( )
+        })
+        writer.close()
 
         // Get evaluation metrics.
-        val testErr = scoreAndLabels.filter( r => r._2 != r._3 ).count( ).toDouble / testData.count( )
-        println( "Test Error = " + testErr )
+        val testErr = scoreAndLabels.filter(r => r._2 != r._3).count().toDouble / testData.count()
+        println("Test Error = " + testErr)
 
         //model.save(sc,"F:\\data\\lotteryodds\\model\\LogisticRegressionBinary")
-        sc.stop( )
+        sc.stop()
     }
 }
