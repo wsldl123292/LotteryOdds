2e70f9b3142e6515128851dd528eca6c2174b730
LDL <wsldl123292@163.com>
2015-12-17 10:54:17 +0800

    Revert "no message"
    This reverts commit fed6b59fb8e8e2e1d9f691875331448f3bcdc632.

--- src/main/java/com/ldl/lotteryodds/train/TestTextBinary.java
+++ src/main/java/com/ldl/lotteryodds/train/TestTextBinary.java
@@ -32,8 +32,8 @@ import java.util.Map;
 public class TestTextBinary {
     public static void main(String[] args) throws IOException {
 //采集开始时间2011-07-21
-        LocalDate beginDate = LocalDate.of(2015, 11, 28);
-        int size = 30;
+        LocalDate beginDate = LocalDate.of(2015, 11, 22);
+        int size = 83;
         final CloseableHttpClient client = HttpClientBuilder.create().build();
         CloseableHttpResponse response;
         HttpGet get;
@@ -93,7 +93,7 @@ public class TestTextBinary {
 
             //解析每个tr转换为实体
             final Elements trs = tableMatch.select("tbody>tr");
-            for (int i = 0; i < trs.size(); i++) {
+            for (int i = 0; i < size; i++) {
             //for (Object tr : trs) {
                 final Element element = trs.get(i);
                 if (!element.attr("parentid").trim().equals("")) {
